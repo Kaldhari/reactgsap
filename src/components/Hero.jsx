48a964ded5e72b0b3560dba8329317b6ed5c8e90
sriharsha222 <harsha_k_s@vulcantech.com>
2025-08-15 01:08:57 +0530

    This is the Hero Section of the page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useState,useRef } from 'react';
-
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
    const [currentIndex,setCurrentIndex] = useState(1);
@@ -51,12 +52,32 @@ const Hero = () => {
 
             <video
             src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-            autoPlayloop
+            autoPlay
+            loop
             muted
-            className="absolute left-0 top-0 size-full object-cover object-center"/>
+            className="absolute left-0 top-0 size-full object-cover object-center"
+            onLoadedData={handleVideoLoad}/>
 
            </div>
+           <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+              G<b>a</b>ming
+           </h1>
+
+           <div className="absolute left-0 top-0 z-40 size-full">
+                <div className="mt-24 px-5 sm:px-10">
+                  <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e  </h1>
+                  <p className="mb-5 max-w-md font-robert-regular text-blue-100 text-sm sm:text-base">Enter the Metagame Layer<br/>
+                  Unleash the Play Economy</p>
+                <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="bg-[#faff00] text-black flex items-center gap-2 font-semibold rounded-full px-6 py-2 transition-colors"/>
+
+               
+                </div>
+           </div>
       </div>
+
+      <h1 className="special-font hero-heading absolute bottom-5 right-5  text-black">
+              G<b>a</b>ming
+           </h1>
     </div>
   )
 }
